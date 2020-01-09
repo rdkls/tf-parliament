d496930200fd4610e411bdf498f0ad2166d0b1a6
Nick Doyle <nick.doyle@intelematics.com>
2020-01-09 12:09:15 +1100

    feat: add test fixture

---
+++ tests/aws_iam_policy_lambda.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_policy" "lambda_logging" {
+  name = "lambda_logging"
+  path = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
