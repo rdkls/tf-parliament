83b0fc3d00cd8b3207b9bcfe7b965d73850ee1d1
Nick Doyle <nick.doyle@intelematics.com>
2020-01-09 12:03:15 +1100

    feat: refactor, cleanup, support aws_iam_policy resources, nicer detail formatting

--- tf-parliament.py
+++ tf-parliament.py
@@ -8,11 +8,6 @@ import json
 import parliament
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename_list', nargs='+')
-parser.add_argument('-q', '--quiet', action='store_true', default=False, help="Quiet mode - only print if policy errors")
-args = parser.parse_args()
-
 statement_template = """{{
     "Effect": "{effect}",
     "Action": {actions}
@@ -22,15 +17,15 @@ statement_template = """{{
 policy_template = """{{
     "Version": "2012-10-17",
     "Id": "123",
-    "Statement": {statements}
+    "Statement": {iam_statements}
 }}"""
 
 field_mappings = [
-    {'tf_key': 'effect', 'iam_key': 'Effect', 'mock_var': 'Allow'},
-    {'tf_key': 'actions', 'iam_key': 'Action', 'mock_var': '*'},
-    {'tf_key': 'not_actions', 'iam_key': 'NotAction', 'mock_var': '*'},
-    {'tf_key': 'resources', 'iam_key': 'Resource', 'mock_var': '*'},
-    {'tf_key': 'not_resources', 'iam_key': 'NotResource', 'mock_var': '*'},
+    {'tf_key': 'effect', 'iam_key': 'Effect', 'mock_value': 'Allow'},
+    {'tf_key': 'actions', 'iam_key': 'Action', 'mock_value': '*'},
+    {'tf_key': 'not_actions', 'iam_key': 'NotAction', 'mock_value': '*'},
+    {'tf_key': 'resources', 'iam_key': 'Resource', 'mock_value': '*'},
+    {'tf_key': 'not_resources', 'iam_key': 'NotResource', 'mock_value': '*'},
 ]
 
 
@@ -45,70 +40,113 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-findings_found = False
-
-
 def format_finding(f):
-    return f'{bcolors.WARNING}{f.issue}{bcolors.ENDC} - {f.detail} - {f.location}'
+    if isinstance(f.detail, list):
+        details_formatted = []
+        for d in f.detail:
+            detail_formatted = ', '.join([f'{k}: {d[k]}' for k in d.keys()])
+            details_formatted.append(detail_formatted)
+        details_formatted = "\n  ".join(details_formatted)
+    else:
+        details_formatted = f.detail
+
+    return f'{bcolors.WARNING}{f.issue}{bcolors.ENDC}\nDetails:\n  {details_formatted}\nLocation:\n  {f.location}'
 
 
-for filename in args.filename_list:
-    # If directory specified, scan all files in that dir
-    if os.path.isdir(filename):
-        filename = os.path.join(filename, '*')
+def mock_iam_statement_from_tf(statement_data):
+    # Create a mock IAM statement from a TF definition,
+    # copying across only fields defined in the field_mappings
+    # and replacing TF interpolations "${var.xxx}"
+    # with mock vars for the field to pass validation
+    mock_iam_statement = {}
 
-    # Expand wildcards
-    for filename in glob.glob(filename):
-        # Only operate on .tf
-        if not filename.endswith('.tf'):
+    # In TF, effect is optional and defaults to 'Allow'
+    try:
+        mock_iam_statement['Effect'] = statement_data['effect']
+    except KeyError:
+        mock_iam_statement['Effect'] = 'Allow'
+
+    for field in field_mappings:
+        if statement_data.get(field['tf_key'], None):
+            field_values = statement_data.get(field['tf_key'])[0]
+
+            if isinstance(field_values, list):
+                field_values = list(map(lambda x: re.sub('\${.*?}', field['mock_value'], x), field_values))
+            else:
+                field_values = re.sub('\${.*?}', field['mock_value'], field_values)
+
+            mock_iam_statement[field['iam_key']] = field_values
+    return mock_iam_statement
+
+
+def validate_file(filename):
+    with(open(filename, 'r')) as file:
+        tf = hcl2.load(file)
+
+    findings = []
+
+    # Validate data.aws_iam_policy_document
+    for policy_document in filter(lambda x: x.get('aws_iam_policy_document', None), tf.get('data', [])):
+        iam_statements = []
+        policy_name = list(policy_document['aws_iam_policy_document'].keys())[0]
+        for statement_data in policy_document['aws_iam_policy_document'][policy_name]['statement']:
+            # Don't check assume role policies; these will have spurious findings for
+            # "Statement contains neither Resource nor NotResource"
+            actions = statement_data.get('actions')[0]
+            if actions == ['sts:AssumeRole'] or actions == ['sts:AssumeRoleWithSAML']:
+                continue
+
+            iam_statements.append(mock_iam_statement_from_tf(statement_data))
+
+        policy_string = policy_template.format(iam_statements=json.dumps(iam_statements))
+        findings += parliament.analyze_policy_string(policy_string).findings
+
+    # Validate resource.aws_iam_policy
+    for policy in filter(lambda x: x.get('aws_iam_policy', None), tf.get('resource', [])):
+        try:
+            policy_string = policy['aws_iam_policy']['policy']['policy'][0]
+        except KeyError:
             continue
 
-        with(open(filename, 'r')) as file:
-            tf = hcl2.load(file)
+        findings += parliament.analyze_policy_string(policy_string).findings
 
-        findings = []
-        for policy_data in filter(lambda x: x.get('aws_iam_policy_document', None), tf.get('data', [])):
-            statements = []
-            policy_name = list(policy_data['aws_iam_policy_document'].keys())[0]
-            for statement_data in policy_data['aws_iam_policy_document'][policy_name]['statement']:
-                # Don't check assume role policies; these will have spurious findings for
-                # "Statement contains neither Resource nor NotResource"
-                actions = statement_data.get('actions')[0]
-                if actions==['sts:AssumeRole'] or actions==['sts:AssumeRoleWithSAML']:
-                    continue
+    return findings
 
-                statement = {}
 
-                for field in field_mappings:
-                    if statement_data.get(field['tf_key'], None):
-                        field_values = statement_data.get(field['tf_key'])[0]
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename_list', nargs='+')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help="Quiet mode - only print if policy errors")
+    parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    args = parser.parse_args()
 
-                        # If there are TF vars in the string e.g. "${var.xxx}"
-                        # we replace these with "mock" vars for the field to pass validation
-                        if list == type(field_values):
-                            field_values = list(map(lambda x: re.sub('\${.*?}', field['mock_var'], x), field_values))
-                        else:
-                            field_values = re.sub('\${.*?}', field['mock_var'], field_values)
+    # Bool to indicate if any findings, for all files - will be used for exit code
+    findings_found = False
 
-                        statement[field['iam_key']] = field_values
+    for filename in args.filename_list:
+        # If directory specified, scan all files in that dir
+        if os.path.isdir(filename):
+            filename = os.path.join(filename, '*')
 
-                statements.append(statement)
+        # Expand wildcards
+        for filename in glob.glob(filename):
+            # Only operate on .tf
+            if not filename.endswith('.tf'):
+                continue
 
-            policy_string = policy_template.format(statements=json.dumps(statements))
-            analyzed_policy = parliament.analyze_policy_string(policy_string)
+            findings = validate_file(filename)
+            if findings:
+                print(f"{bcolors.FAIL}{filename}{bcolors.ENDC}")
+                for f in findings:
+                    print(format_finding(f))
+                    findings_found = True
+                print()
+            elif args.verbose:
+                print(f"{bcolors.OKGREEN}{filename} VALID{bcolors.ENDC}")
 
-            if analyzed_policy.findings:
-                findings += analyzed_policy.findings
-                
-        if findings:
-            print(f"{bcolors.FAIL}{filename}{bcolors.ENDC}")
-            for f in findings:
-                print(format_finding(f))
-                findings_found = True
-            print()
-
-if findings_found:
-    sys.exit(1)
-else:
-    if not args.quiet:
-        print(f"{bcolors.OKGREEN}No errors found{bcolors.ENDC}")
+    # If we found any findings, in any files, exit code non-zero
+    if findings_found:
+        sys.exit(1)
+    else:
+        if not args.quiet:
+            print(f"{bcolors.OKGREEN}No errors found{bcolors.ENDC}")
