edcdce08c96343d4d77ef594f7daf2c1c97ee4bd
Nick Doyle <nick.doyle@intelematics.com>
2020-05-21 16:38:34 +1000

    feat: add test file for aws_iam_policy with single line

---
+++ tests/aws_iam_policy_single_line.tf
@@ -0,0 +1,4 @@
+ resource "aws_iam_policy" "example" {
+   # ... other configuration ...
+   policy = "{\"Version\": \"2012-10-17\", \"Statement\": {\"Effect\": \"Allow\", \"Action\": \"*\", \"Resource\":   \"*\"}}"
+ }
